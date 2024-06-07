2fe5645ed856a19833a637077a50df87c3cdba5b
vinibispo <vini.bispo015@gmail.com>
2024-06-06 23:44:37 -0300

    Implement HealthCheck

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error>  {
+    let address = format!("127.0.0.1:8000");
+    let listener = TcpListener::bind(address).expect("Failed to bind random port");
+    run(listener)?.await
 }
