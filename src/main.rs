45926b2de35c45b6fe391268d8029f61a5d9043d
vinibispo <vini.bispo015@gmail.com>
2024-06-07 05:06:43 -0300

    Add subscriptions endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use zero2prod::run;
 use std::net::TcpListener;
+use zero2prod::run;
 
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error>  {
+async fn main() -> Result<(), std::io::Error> {
     let address = format!("127.0.0.1:8000");
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
     run(listener)?.await
